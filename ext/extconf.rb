045ef6aef570c55e38fb59b576ab8508652b8f69
koyachi <rtk2106@gmail.com>
2008-11-21 10:09:36 +0900

    reduce library dependencies

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 require 'mkmf'
 
-%w[Magick++ Magick Wand zebra].each do |header|
+%w[Magick++ Magick zebra].each do |header|
   dir_config(header)
 end
 
+$CFLAGS =     ENV['CFLAGS'].to_s   + ' ' + `Magick-config --cflags`.chomp
+$CPPFLAGS =   ENV['CPPFLAGS'].to_s + ' ' + `Magick-config --cppflags`.chomp
+$LDFLAGS =    ENV['LDFLAGS'].to_s  + ' ' + `Magick-config --ldflags`.chomp
+$LOCAL_LIBS = ENV['LIBS'].to_s     + ' ' + `Magick-config --libs`.chomp
+
 depends = []
-%w[stdc++ Magick++ Magick Wand zebra].each do |lib|
+%w[stdc++ Magick++ Magick zebra].each do |lib|
   depends << have_library(lib)
 end
 
-%w[string magick/MagickCore zebra].each do |header|
+%w[string zebra].each do |header|
   depends << have_header("#{header}.h")
 end
 if depends.all? then
